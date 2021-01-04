bf542360893c163070621bb338991d873adefb6c
vkguru <vnwoguru@gmail.com>
2021-01-04 12:45:59 +0100

    update meta data

--- components/header.js
+++ components/header.js
@@ -10,7 +10,8 @@ export default function Header({siteTitle}) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <meta name="msapplication-TileImage" href="/fav-270x270.png" />
-        <meta name="description" content="Portfolio site of Victor Nwoguru, a frontend developer who is super skilled at his craft" />
+        <meta name="description" content="Victor Nwoguru is a frontend developer who is passionate about building amazing digital products and also sharing his knowledge with fellow devs" />
+        <meta name="keywords" content="Frontend Developer, Frontend Engineer, Software Engineer, Technical Writer, Content Creator, Web Developer"></meta>
         <meta
           property="og:image"
           content="/fav-270x270.png"
