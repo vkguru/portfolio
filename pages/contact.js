b94e67b9024a6be90adaecd33b040fa89c0b874b
vkguru <vnwoguru@gmail.com>
2021-03-15 18:22:26 +0100

    update contact

--- pages/contact.js
+++ pages/contact.js
@@ -18,7 +18,7 @@ export default function Contact() {
           <h2>Contact</h2>
 
           <div className={styles.socialC}>
-            <p>I will love you to connect with me. You can find me on;</p>
+            <p>I will love you to connect with me. You can connect with me via;</p>
             <ul>
               <li><Link href="https://twitter.com/vichukwu"><a target="_blank">Twitter</a></Link></li>
               <li><Link href="https://www.linkedin.com/in/victor-nwoguru/"><a target="_blank">LinkedIn</a></Link></li>
